6a9037b63e9f79d52966f8b96d9bd758f512666b
fumigoro <fumigoro@FUMI-DESKTOP.localdomain>
2020-11-19 23:12:43 +0900

    クイズページでも動作するように

--- DandD-extension-for-aims/contents.js
+++ DandD-extension-for-aims/contents.js
@@ -2,10 +2,12 @@ const uploadAreaTemp = document.createElement('div');
 uploadAreaTemp.className = 'file_drop_area';
 uploadAreaTemp.innerHTML = 'ファイルをドラックしてアップロード';
 
-const button = document.getElementsByClassName('Button Button--primary submit_assignment_link');
+const button_assingment = document.getElementsByClassName('Button Button--primary submit_assignment_link');
+const filebox_quiz = document.getElementsByClassName('content-box border border-trbl file-upload-box');
+
 const cancelButton = document.getElementsByClassName('cancel_button btn');
 
-if (button[0]) {
+if (button_assingment[0]) {//課題ページ
     window.addEventListener('load', () => {
 
         const formArea = document.getElementsByClassName('submission_attachment');
@@ -41,9 +43,33 @@ if (button[0]) {
             });
         }
     });
-    cancelButton[0].addEventListener('click', () => {
-        document.getElementsByClassName('file_drop_area');
-    })
-
     
 };
+if (filebox_quiz[0]) {//クイズページかつファイルアップロード形式の問題がある
+    window.addEventListener('load', () => {
+        console.log(filebox_quiz);
+        for(let i=0;i<filebox_quiz.length;i++){
+            filebox_quiz[i].appendChild(uploadAreaTemp.cloneNode(true));
+        }
+
+        const uploadArea = document.getElementsByClassName('file_drop_area');
+        for (let i=0;i<uploadArea.length;i++) {
+            //ドラッグしホーバーしている状態で実行
+            uploadArea[i].addEventListener('dragover', (event) => {
+                event.preventDefault();
+                event.target.classList.add('drag');
+            });
+            //ホーバーしているカーソルがD&Dエリアを出たとき実行
+            uploadArea[i].addEventListener('dragleave', (event) => {
+                event.target.classList.remove('drag');
+            });
+            //ドロップされたとき実行
+            uploadArea[i].addEventListener('drop', (event) => {
+                event.target.classList.remove('drag');
+                event.preventDefault();
+                const input = document.getElementsByClassName('question_input file-upload hidden');
+                input[i].files = event.dataTransfer.files;
+            });
+        }
+    });
+}
